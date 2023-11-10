7814874b0b9e23768aa6ccbd6b54dbd5505ecda6
DaviGel <geloso.davide@gmail.com>
2023-11-10 11:27:57 +0100

    fix: sistemata stampa

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -6,11 +6,11 @@ const parola1 = prompt('Inserisci la prima parola:');
 const parola2 = prompt('Inserisci la seconda parola:');
 
 if (parola1.length > parola2.length) {
-  console.log(parola1);
   console.log(parola2);
+  console.log(parola1);
 } else if (parola2.length > parola1.length) {
-  console.log(parola2);
   console.log(parola1);
+  console.log(parola2);
 } else {
   console.log('Le parole hanno la stessa lunghezza');
 }
