8d622637060586ad29c0ab62dfe6cb1ed14758fe
DaviGel <geloso.davide@gmail.com>
2023-11-14 12:37:56 +0100

    feat: aggiunto number al prompt

--- snack5/js/main.js
+++ snack5/js/main.js
@@ -5,7 +5,7 @@ Fai inserire un numero, che chiameremo N, all’utente.
 Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo
 */
 
-const N = prompt('Inserisci un numero:');
+const N = Number(prompt('Inserisci un numero:'));
 
 for (let i = 0; i < N; i++) {
   const array = [];
