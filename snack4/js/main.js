2a7387ec27ccd56ee8647eb1c41e3999fcf634b9
DaviGel <geloso.davide@gmail.com>
2023-11-14 11:03:33 +0100

    feat: snack4

--- snack4/js/main.js
+++ snack4/js/main.js
@@ -7,16 +7,25 @@ const array = [];
 let somma = 0;
 let i = 0;
 
-while (somma < 50) {
-  array.push(Number(prompt('Inserisci un numero:')));
-  if (!isNaN(array[i])) {
-    somma += array[i];
+// while (somma < 50) {
+//   array.push(Number(prompt('Inserisci un numero:')));
+//   if (!isNaN(array[i])) {
+//     somma += array[i];
+//   } else {
+//     console.log(
+//       'Hai inserito un carattere che non verrà conteggiato nella somma.'
+//     );
+//   }
+//   i++;
+// }
+
+do {
+  i = Number(prompt('Inserisci un numero:'));
+  if (!isNaN(i)) {
+    somma += i;
   } else {
-    console.log(
-      'Hai inserito un carattere che non verrà conteggiato nella somma.'
-    );
+    console.log('Hai inserito un valore che non verrà conteggiato nella somma');
   }
-  i++;
-}
+} while (somma < 50);
 
 console.log('La somma è dei numeri inseriti è', somma);
