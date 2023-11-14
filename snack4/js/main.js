084c8d8160fbcf11e171abc77b988d7c20c785f3
DaviGel <geloso.davide@gmail.com>
2023-11-14 09:48:34 +0100

    feat: snack4

---
+++ snack4/js/main.js
@@ -0,0 +1,22 @@
+/*
+Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è inferiore di 50.
+*/
+
+const array = [];
+let somma = 0;
+let i = 0;
+
+while (somma < 50) {
+  array.push(Number(prompt('Inserisci un numero:')));
+  if (!isNaN(array[i])) {
+    somma += array[i];
+  } else {
+    console.log(
+      'Hai inserito un carattere che non verrà conteggiato nella somma.'
+    );
+  }
+  i++;
+}
+
+console.log('La somma è dei numeri inseriti è', somma);
